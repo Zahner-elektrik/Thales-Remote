5d55ddb3de752a91aa49386dc618a16e79ccac6c
DF9IR <50953001+DF9IR@users.noreply.github.com>
2019-05-24 10:22:14 +0200

    + added header with license * polished the example in main.c a little bit

--- main.cpp
+++ main.cpp
@@ -1,4 +1,30 @@
-﻿#include <thread>
+﻿/******************************************************************
+ *  ____       __                        __    __   __      _ __
+ * /_  / ___ _/ /  ___  ___ ___________ / /__ / /__/ /_____(_) /__
+ *  / /_/ _ `/ _ \/ _ \/ -_) __/___/ -_) / -_)  '_/ __/ __/ /  '_/
+ * /___/\_,_/_//_/_//_/\__/_/      \__/_/\__/_/\_\\__/_/ /_/_/\_\
+ *
+ * Copyright 2019 ZAHNER-elektrik I. Zahner-Schiller GmbH & Co. KG
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software
+ * is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+ * THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include <thread>
 
 #include "thalesremoteconnection.h"
 #include "thalesremotescriptwrapper.h"
@@ -59,12 +85,12 @@ void spectrum(ThalesRemoteScriptWrapper &scriptHandle, double lower_frequency, d
         // calculating logarithmic equidistant frequencies
         double current_frequency = std::exp(log_lower_frequency + log_interval_spacing * static_cast<double>(i));
 
-        std::cout << "frequency " << current_frequency << std::endl;
+        std::cout << "Frequency " << current_frequency << std::endl;
         printImpedance(scriptHandle.getImpedance(current_frequency));
     }
 }
 
 void printImpedance(std::complex<double> impedance) {
 
-    std::cout << std::abs(impedance) << "ohm, " << std::arg(impedance) << "rad" << std::endl;
+    std::cout << std::abs(impedance) << " ohm, " << std::arg(impedance) << " rad" << std::endl;
 }
